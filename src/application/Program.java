b63d55ccf020fd7b6934240ceddc840740d823f2
Gabriel D'Andréa <webgabrielaraujo@gmail.com>
2023-03-14 16:13:28 -0300

    Quarta Versão: Implementando através de expressão lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class Program {
 
@@ -18,8 +18,14 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
+		
+		double updatePrice = 1.1;
 
-		list.forEach(Product::priceUpdate);
+		Consumer<Product> consumer = p -> {
+			p.setPrice(p.getPrice() * updatePrice);	
+		};
+		
+		list.forEach(consumer);
 		
 		list.forEach(System.out::println);
 		
